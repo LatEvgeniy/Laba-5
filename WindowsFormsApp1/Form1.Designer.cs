44e855e37f0ee06c5f7c81706c0044c8fa2bbd94
LatEvgeniy <e.latinin48@gmail.com>
2023-03-25 12:59:24 +0200

    First commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBuy = new System.Windows.Forms.Button();
+            this.NumUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btSell = new System.Windows.Forms.Button();
+            this.lbProducts = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDownSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btBuy
+            // 
+            this.btBuy.Location = new System.Drawing.Point(464, 79);
+            this.btBuy.Name = "btBuy";
+            this.btBuy.Size = new System.Drawing.Size(157, 38);
+            this.btBuy.TabIndex = 0;
+            this.btBuy.Text = "Запуск потоку переобліку";
+            this.btBuy.UseVisualStyleBackColor = true;
+            // 
+            // NumUpDownSeconds
+            // 
+            this.NumUpDownSeconds.Location = new System.Drawing.Point(464, 53);
+            this.NumUpDownSeconds.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.NumUpDownSeconds.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumUpDownSeconds.Name = "NumUpDownSeconds";
+            this.NumUpDownSeconds.Size = new System.Drawing.Size(29, 20);
+            this.NumUpDownSeconds.TabIndex = 4;
+            this.NumUpDownSeconds.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(461, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Час на протязі якого магазин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "поповнюється товарами (сек)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "зі швидкістью: 1 товар / сек";
+            // 
+            // btSell
+            // 
+            this.btSell.Location = new System.Drawing.Point(462, 169);
+            this.btSell.Name = "btSell";
+            this.btSell.Size = new System.Drawing.Size(157, 38);
+            this.btSell.TabIndex = 8;
+            this.btSell.Text = "Запуск потоку дій покупців";
+            this.btSell.UseVisualStyleBackColor = true;
+            // 
+            // lbProducts
+            // 
+            this.lbProducts.FormattingEnabled = true;
+            this.lbProducts.Location = new System.Drawing.Point(12, 9);
+            this.lbProducts.Name = "lbProducts";
+            this.lbProducts.Size = new System.Drawing.Size(444, 199);
+            this.lbProducts.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 219);
+            this.Controls.Add(this.lbProducts);
+            this.Controls.Add(this.btSell);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumUpDownSeconds);
+            this.Controls.Add(this.btBuy);
+            this.Name = "Form1";
+            this.Text = "Магазин телефонів";
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDownSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBuy;
+        private System.Windows.Forms.NumericUpDown NumUpDownSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btSell;
+        private System.Windows.Forms.ListBox lbProducts;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static Thread buyingProductsThread;
+        static Thread sellingProductsThread;
+
+        Random myrandom = new Random();
+
+        Store store = new Store();
+        Dictionary<string, double> availableProducts = new Dictionary<string, double>()
+        {
+            {"iPhone 13", 950.50 }, {"iPhone 13 Pro Max", 1050.50 }, {"Pixel 6 Pro", 850.50}, 
+            {"Galaxy S21 FE 5G", 920.50 }, {"Galaxy S21 Ultra", 950.50 }, {"Galaxy Z Flip 3 5G", 1150.50 }, 
+            {"Galaxy Z Fold3 5G", 1120.50 }, {"Xiaomi 11T Pro", 750.50 }, {"Xiaomi Mi 11 Ultra", 720.50},
+            {"ASUS ROG Phone 5s Pro", 700.50 }
+        };
+
+        List<string> productNames = new List<string>()
+        {
+            "iPhone 13", "iPhone 13 Pro Max", "Pixel 6 Pro", "Galaxy S21 FE 5G",
+            "Galaxy S21 Ultra", "Galaxy Z Flip 3 5G", "Galaxy Z Fold3 5G",
+            "Xiaomi 11T Pro", "Xiaomi Mi 11 Ultra", "ASUS ROG Phone 5s Pro"            
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            btBuy.Click += BtInventory_Click;
+            btSell.Click += BtSell_Click;
+        }
+
+        private void BtSell_Click(object sender, EventArgs e)
+        {
+            sellingProductsThread = new Thread(RandomSellingProducts);
+            sellingProductsThread.Start();
+        }
+
+        private void BtInventory_Click(object sender, EventArgs e)
+        {
+            buyingProductsThread = new Thread(RandomBuyingProducts);
+            buyingProductsThread.Start();
+        }
+
+        public void RandomSellingProducts()
+        {
+            int clientsNum = myrandom.Next(1, 5);
+
+            for (int i = 0; i < clientsNum; i++)
+            {
+                string name = productNames[myrandom.Next(0, productNames.Count)];
+                int productCount = myrandom.Next(1, 10);
+
+                store.SellProduct(name, productCount);
+
+                lbProducts.Items.Clear();
+                foreach (var product in store.storage)
+                    lbProducts.Items.Add(product);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void RandomBuyingProducts()
+        {
+            decimal productsNum = NumUpDownSeconds.Value;
+
+            for (int i = 0; i < productsNum; i++)
+            {
+                string name = productNames[myrandom.Next(0, productNames.Count)];
+                int productCount = myrandom.Next(1, 10);
+                double purchasePrice = availableProducts[name];
+                double sellPrice = Math.Round(purchasePrice * 1.15, 2);
+
+                store.BuyProduct(new Product(name, productCount, purchasePrice, sellPrice));
+
+                lbProducts.Items.Clear();
+                foreach (var product in store.storage)
+                    lbProducts.Items.Add(product);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    
+    public class Product
+    {
+        private string name;
+        private int productCount;
+        private double purchasePrice;
+        private double sellPrice;
+
+        public string Name { get => name; set => name = value; }
+        public int ProductCount
+        {
+            get => productCount;
+            set
+            {
+                if (value >= 0)
+                    productCount = value;
+            }
+        }
+
+        public double PurchasePrice { get => purchasePrice; set => purchasePrice = value; }
+        public double SellPrice { get => sellPrice; set => sellPrice = value; }
+
+        public Product(string name, int productCount, double purchasePrice, double sellPrice)
+        {
+            Name = name;
+            ProductCount = productCount;
+            PurchasePrice = purchasePrice;
+            SellPrice = sellPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} з ціною покупки {PurchasePrice} та ціною продажу {SellPrice} у кількості {ProductCount}";
+        }
+
+        public Product() { }
+    }
+
+    public class Store
+    {
+        private double amountOfMoney = 100000;
+
+        public double AmountOfMoney 
+        { 
+            get => amountOfMoney; 
+            set
+            {
+                if (value >= 0)
+                    amountOfMoney = value;
+            }
+        }
+
+
+        public List<Product> storage = new List<Product>();
+
+        public void SellProduct(string productName, int productCount)
+        {
+            foreach (Product storage_product in storage)
+            {
+                if (storage_product.Name == productName && storage_product.ProductCount - productCount > 0)
+                {
+                    AmountOfMoney += storage_product.SellPrice;
+                    storage_product.ProductCount -= productCount;
+
+                    string[] output = { DateTime.Now.ToString(), $"{storage_product.ToString()} через те що їх щойно продали {productCount}"};
+
+                    var serializer = new XmlSerializer(typeof(string[]));
+                    using (var file = new FileStream("log.xml", FileMode.Append))
+                        serializer.Serialize(file, output);
+                }
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            bool isAdded = false;
+            foreach (Product storage_product in storage)
+            {
+                if (storage_product.Name == product.Name && AmountOfMoney - product.PurchasePrice >= 0)
+                {
+                    AmountOfMoney -= product.SellPrice;
+                    storage_product.ProductCount += product.ProductCount;
+
+                    string[] output = { DateTime.Now.ToString(), "Магазин купив: " + product.ToString() + $" у кількості {product.ProductCount}"};
+                    
+                    //File.AppendAllLines("logging.log", output);
+                    var serializer = new XmlSerializer(typeof(string[]));
+                    using (var file = new FileStream("log.xml", FileMode.Append))
+                        serializer.Serialize(file, output);
+                    
+                    isAdded = true;
+                }
+            }
+            if (!isAdded && AmountOfMoney - product.PurchasePrice >= 0)
+            {
+                AmountOfMoney -= product.PurchasePrice;
+                storage.Add(product);
+
+                string[] output = { DateTime.Now.ToString(), "Магазин купив: " + product.ToString() + $" у кількості {product.ProductCount}"};
+
+                var serializer = new XmlSerializer(typeof(string[]));
+                using (var file = new FileStream("log.xml", FileMode.Append))
+                    serializer.Serialize(file, output);
+            }
+        }
+
+        public Store() { }
+    }
+
+}
